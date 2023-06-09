8986be0a81fd383b9692ae7b6c42e28810ee2e3f
Sergey Antonyuk <s.antonyuk@white-soft.ru>
2023-06-09 20:35:50 +1000

    add fetcher.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+
+const { fetcher } = require('./fetcher.js');
+
+/*
+    TODO: краулер страницы
+    POST http://localhost:3000/parse
+    body: { domainName: string}
+    return string[]
+*/
 const app = express();
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 app.post("/parse", function(request, response){
-    response.send(`<h2>${request.body.domainName}</h2>`);
+    response.send(fetcher(request.body.domainName));
 });
 
 app.listen(3000);
