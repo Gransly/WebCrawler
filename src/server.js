b3867a8bd42e1f3c675bef771160bd5bace76134
Sergey Antonyuk <s.antonyuk@white-soft.ru>
2023-06-10 18:00:57 +1000

    implement /parse endpoint

--- src/server.js
+++ src/server.js
@@ -1,20 +1,33 @@
 const express = require("express");
 
-const { fetcher } = require('./fetcher.js');
+const { getLinks } = require('./utils/linksUtils');
+const { differenceInSet } = require('./utils/setUtils');
 
-/*
-    TODO: краулер страницы
-    POST http://localhost:3000/parse
-    body: { domainName: string}
-    return string[]
-*/
 const app = express();
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 
-app.post("/parse", function(request, response){
-    response.send(fetcher(request.body.domainName));
+app.post("/parse", async function (request, response) {
+    let links = new Set();
+    let fetchedLinks = new Set();
+
+    const arrayLinks = await getLinks(request.body.domainName);
+    arrayLinks.forEach(value => links.add(value));
+    fetchedLinks.add(request.body.domainName);
+
+    while (differenceInSet(links, fetchedLinks).length !== 0) {
+        const linkToFetch = differenceInSet(links, fetchedLinks)[0];
+        const arrayLinks = await getLinks(linkToFetch);
+        if(arrayLinks){
+            arrayLinks.forEach(value => links.add(value));
+        }
+        fetchedLinks.add(linkToFetch);
+    }
+
+    response.send(Array.from(links));
 });
 
+
 app.listen(3000);
+
